1868622c1538221f45345460b0d98883e056f6b4
Faliell <fabiosw@me.com>
2022-10-12 16:31:07 +0100

    lesson if

--- src/App.svelte
+++ src/App.svelte
@@ -15,6 +15,9 @@
 	{#each people as person (person.id)}
 		<div>
 			<h4>{person.name}</h4>
+			{#if person.beltColour === "black"}
+				<p><strong>Master Ninja</strong></p>
+			{/if}
 			<p>{person.age} years old, {person.beltColour} belt.</p>
 			<button on:click={() => handleClick(person.id)}>delete</button>
 		</div>
